c19c50984fc8d4e57f0c8316b7d3ceb2737c8052
pakoo <zealzpc@gmail.com>
2013-01-21 02:54:25 -0500

    haha

--- crawler.py
+++ crawler.py
@@ -7,7 +7,7 @@ import tweepy
 con = MongoClient('localhost',27017)
 db = con.air.pm
 
-shanghai = tweepy.api.get_user('CGShanghaiAire')
+shanghai = tweepy.api.get_user('CGShanghaiAir')
 beijing = tweepy.api.get_user('BeijingAir')
 chengdu = tweepy.api.get_user('CGChengduAir')
 guangzhou = tweepy.api.get_user('Guangzhou_Air')
@@ -24,7 +24,7 @@ def parserdata(tweet,place):
     解析美领馆pm2.5的数据
     """
     create_time = tweet.created_at
-    data = tweet.split(';')[2]
+    data = tweet.text.split(';')[2]
     res = db.find_one({'location':place,'create_time':create_time}) 
     if not res:
         print ">>>>>>>>发现一条 %s %s 的新数据 PM2.5=%s "%(place,str(create_time),data)
